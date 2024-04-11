64a8c94ad0168209d5d184e06f0dce5bd7fa4faa
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-04-11 17:42:55 +0200

    add region and group requirement

--- src/api/main/java/com/ampznetwork/worldmod/api/WorldMod.java
+++ src/api/main/java/com/ampznetwork/worldmod/api/WorldMod.java
@@ -1,6 +1,14 @@
 package com.ampznetwork.worldmod.api;
 
+import com.ampznetwork.worldmod.api.model.Group;
+import com.ampznetwork.worldmod.api.model.Region;
+
+import java.util.Collection;
+
 public interface WorldMod {
     String AddonId = "worldmod";
     String AddonName = "WorldMod";
+
+    Collection<? extends Region> getRegions();
+    Collection<? extends Group> getGroups();
 }

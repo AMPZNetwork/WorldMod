4c2cf3615170ace468018e19fb2a5b5bd05aad62
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-04-13 22:19:24 +0200

    compiler fix

--- src/fabric/main/java/com/ampznetwork/worldmod/fabric/WorldMod_Fabric.java
+++ src/fabric/main/java/com/ampznetwork/worldmod/fabric/WorldMod_Fabric.java
@@ -1,14 +1,34 @@
 package com.ampznetwork.worldmod.fabric;
 
 import com.ampznetwork.worldmod.api.WorldMod;
+import com.ampznetwork.worldmod.api.model.adp.PlayerAdapter;
+import com.ampznetwork.worldmod.api.model.region.Group;
+import com.ampznetwork.worldmod.api.model.region.Region;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
 
 public class WorldMod_Fabric implements ModInitializer, WorldMod {
     public static final Logger LOGGER = LoggerFactory.getLogger(WorldMod.AddonName);
 
     @Override
     public void onInitialize() {
+    }
+
+    @Override
+    public Collection<? extends Region> getRegions() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends Group> getGroups() {
+        return null;
+    }
+
+    @Override
+    public PlayerAdapter getPlayerAdapter() {
+        return null;
     }
 }
--- src/forge/main/java/com/ampznetwork/worldmod/forge/WorldMod$Forge.java
+++ src/forge/main/java/com/ampznetwork/worldmod/forge/WorldMod$Forge.java
@@ -1,8 +1,27 @@
 package com.ampznetwork.worldmod.forge;
 
 import com.ampznetwork.worldmod.api.WorldMod;
+import com.ampznetwork.worldmod.api.model.adp.PlayerAdapter;
+import com.ampznetwork.worldmod.api.model.region.Group;
+import com.ampznetwork.worldmod.api.model.region.Region;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Collection;
 
 @Mod(WorldMod.AddonName)
 public class WorldMod$Forge implements WorldMod {
+    @Override
+    public Collection<? extends Region> getRegions() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends Group> getGroups() {
+        return null;
+    }
+
+    @Override
+    public PlayerAdapter getPlayerAdapter() {
+        return null;
+    }
 }

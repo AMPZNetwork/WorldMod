c66d7afbd1903dd9b4d5d8f800226e7fd05048ec
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-05-19 13:17:38 +0200

    initialize spatialanchors to certain size

--- src/api/main/java/com/ampznetwork/worldmod/impl/BasicArea.java
+++ src/api/main/java/com/ampznetwork/worldmod/impl/BasicArea.java
@@ -7,6 +7,8 @@ import org.comroid.api.data.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 @Data
 @AllArgsConstructor
@@ -15,7 +17,9 @@ public class BasicArea implements Area {
     private final List<Vector> spatialAnchors;
 
     public Vector[] getSpatialAnchors() {
-        return spatialAnchors.toArray(Vector[]::new);
+        return spatialAnchors.stream()
+                .filter(Objects::nonNull)
+                .toArray(Vector[]::new);
     }
 
     @Data
@@ -23,7 +27,9 @@ public class BasicArea implements Area {
     @AllArgsConstructor
     public static final class Builder implements org.comroid.api.func.ext.Builder<BasicArea> {
         private Shape shape = Shape.Cuboid;
-        private List<Vector> spatialAnchors = new ArrayList<>();
+        private List<Vector> spatialAnchors = new ArrayList<>() {{
+            for(int i=0;i<8;i++)add(null);
+        }};
 
         @Override
         public BasicArea build() {

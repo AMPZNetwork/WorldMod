fea44341f31c2fb9525dcce54d94df0c26c90d45
Kaley <kaleidox@comroid.org>
2024-06-28 18:16:43 +0200

    update member list generation

--- src/core/main/java/com/ampznetwork/worldmod/core/ui/ClaimMenuBook.java
+++ src/core/main/java/com/ampznetwork/worldmod/core/ui/ClaimMenuBook.java
@@ -2,11 +2,17 @@ package com.ampznetwork.worldmod.core.ui;
 
 import com.ampznetwork.worldmod.api.WorldMod;
 import com.ampznetwork.worldmod.api.model.adp.BookAdapter;
+import com.ampznetwork.worldmod.api.model.mini.PlayerRelation;
 import com.ampznetwork.worldmod.api.model.region.Region;
 import lombok.Value;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -14,9 +20,11 @@ import java.util.stream.Stream;
 import static java.util.stream.Stream.concat;
 import static java.util.stream.Stream.of;
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.event.ClickEvent.changePage;
+import static net.kyori.adventure.text.event.ClickEvent.*;
 import static net.kyori.adventure.text.event.HoverEvent.showText;
+import static org.comroid.api.func.util.Streams.atLeastOneOrElseGet;
 import static org.comroid.api.func.util.Streams.cast;
+import static org.comroid.api.text.Capitalization.Title_Case;
 
 @Value
 public class ClaimMenuBook implements BookAdapter {
@@ -78,8 +86,76 @@ public class ClaimMenuBook implements BookAdapter {
     }
 
     private Stream<Component[]> members() {
-        //ClickEvent.suggestCommand("\u0001 64") <- move cursor to position
-        return of((Object) new Component[]{text("members (wip)")}).flatMap(cast(Component[].class));
+        final int pageLimit = 11;
+        final var helper = new Object() {
+            TextComponent header(PlayerRelation type) {
+                var typeNameTitleCase = Title_Case.convert(type.name());
+                return text()
+                        .append(text("[+]")
+                                .color(NamedTextColor.GREEN)
+                                .hoverEvent(showText(text("Add " + typeNameTitleCase)))
+                                .clickEvent(suggestCommand("/region member add \u0001 " + type.name().toLowerCase())))
+                        .append(text(" %s List\n".formatted(typeNameTitleCase))
+                                .color(NamedTextColor.BLACK))
+                        .decorate(TextDecoration.UNDERLINED)
+                        .build();
+            }
+
+            List<@NotNull TextComponent> entries(PlayerRelation type) {
+                var typeNameTitleCase = Title_Case.convert(type.name());
+                return (switch (type) {
+                    case MEMBER -> region.getMemberIDs();
+                    case OWNER -> region.getOwnerIDs();
+                    default -> throw new IllegalStateException("Unexpected value: " + type);
+                }).stream()
+                        .map(id -> {
+                            var name = worldMod.getPlayerAdapter().getName(id);
+                            return text()
+                                    .append(text("[-]")
+                                            .color(NamedTextColor.RED)
+                                            .hoverEvent(showText(text("Remove %s '%s'".formatted(typeNameTitleCase, name))))
+                                            .clickEvent(runCommand("/region member remove " + name)))
+                                    .append(text(" %s\n".formatted(name))
+                                            .color(NamedTextColor.BLACK))
+                                    .build();
+                        })
+                        .collect(atLeastOneOrElseGet(() -> text("- no %ss -".formatted(type.name().toLowerCase()))
+                                .decorate(TextDecoration.ITALIC)))
+                        .toList();
+            }
+        };
+
+        var pages = new ArrayList<TextComponent>();
+        TextComponent page = null;
+        int pageSize = 0;
+        var categories = of(PlayerRelation.OWNER, PlayerRelation.MEMBER)
+                .map(type -> new AbstractMap.SimpleImmutableEntry<>(type, helper.entries(type).iterator()))
+                .iterator();
+
+        while (categories.hasNext()) {
+            var category = categories.next();
+            if (page == null)
+                page = text().build();
+            var header = helper.header(category.getKey());
+            page = page.append(header);
+            var entries = category.getValue();
+
+            while (entries.hasNext()) {
+                var entry = entries.next();
+                page = page.append(entry);
+                pageSize += 1;
+            }
+
+            page = page.append(text("\n"));
+            if (pageSize >= pageLimit) {
+                pages.add(page);
+                page = null;
+            }
+        }
+        if (page != null)
+            pages.add(page);
+
+        return pages.stream().map(comp -> new Component[]{comp});
     }
 
     private Stream<Component[]> flags() {

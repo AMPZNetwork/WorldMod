eb05e6db486d7c0b51002a3b6cbe1a55e98a091e
Kaley <kaleidox@comroid.org>
2024-12-26 12:57:25 +0100

    add hover info to text resources

--- src/api/main/java/com/ampznetwork/worldmod/api/model/TextResourceProvider.java
+++ src/api/main/java/com/ampznetwork/worldmod/api/model/TextResourceProvider.java
@@ -2,14 +2,19 @@ package com.ampznetwork.worldmod.api.model;
 
 import com.ampznetwork.libmod.api.entity.Player;
 import com.ampznetwork.worldmod.api.WorldMod;
+import com.ampznetwork.worldmod.api.game.Flag;
 import com.ampznetwork.worldmod.api.model.log.LogEntry;
 import lombok.Value;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.util.TriState;
 import org.comroid.api.Polyfill;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import static java.util.Optional.*;
 import static net.kyori.adventure.text.Component.*;
@@ -30,9 +35,15 @@ public class TextResourceProvider {
     public Component ofState(TriState triState) {
         // ✅✳❌
         return switch (triState) {
-            case NOT_SET -> text("✳").color(YELLOW);
-            case FALSE -> text("❌").color(RED);
-            case TRUE -> text("✅").color(GREEN);
+            case NOT_SET -> text("✳").color(YELLOW)
+                    .hoverEvent(HoverEvent.showText(text()
+                            .append(text("Value was not changed"))));
+            case FALSE -> text("❌").color(RED)
+                    .hoverEvent(HoverEvent.showText(text()
+                            .append(text("Value was forced to be FALSE"))));
+            case TRUE -> text("✅").color(GREEN)
+                    .hoverEvent(HoverEvent.showText(text()
+                            .append(text("Value was forced to be TRUE"))));
         };
     }
 
@@ -44,17 +55,27 @@ public class TextResourceProvider {
                         .append(text(Polyfill.durationString(Duration.ofMillis(Instant.now()
                                 .minusMillis(log.getTimestamp().toEpochMilli())
                                 .toEpochMilli()), 2)).color(BLUE))
-                        .append(text("]").color(GRAY)))
+                        .append(text("]").color(GRAY))
+                        .hoverEvent(HoverEvent.showText(text()
+                                .append(text(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+                                        .format(LocalDateTime.ofInstant(log.getTimestamp(), ZoneId.systemDefault())))))))
                 .append(text(" "))
                 .append(ofState(log.getResult() == null ? TriState.NOT_SET : log.getResult().getEquivalent()))
                 .append(text(" "))
-                .append(text(log.getAction()).color(AQUA))
+                .append(text(log.getAction()).color(AQUA)
+                        .hoverEvent(HoverEvent.showText(text(ofNullable(Flag.VALUES.getOrDefault(log.getAction(), null))
+                                .map(Flag::getDescription)
+                                .orElse("Flag could not be matched\nIs a dependent addon missing?")))))
                 .append(text(" @ "))
-                .append(text(ofNullable(log.getTarget()).map(Player::getName).orElseGet(log::getNonPlayerTarget)).color(GREEN));
+                .append(text(ofNullable(log.getTarget()).map(Player::getName).orElseGet(log::getNonPlayerTarget)).color(GREEN)
+                        .hoverEvent(HoverEvent.showText(text()
+                                .append(text("Affected entity")))));
         if (log.getPlayer() != null || log.getNonPlayerSource() != null)
-            text.append(text(" (by "))
-                    .append(text(ofNullable(log.getPlayer()).map(Player::getName).orElseGet(log::getNonPlayerSource)).color(YELLOW))
-                    .append(text(")"));
+            text.append(text(" (by ")
+                    .append(text(ofNullable(log.getPlayer()).map(Player::getName).orElseGet(log::getNonPlayerSource)).color(YELLOW)
+                            .hoverEvent(HoverEvent.showText(text()
+                                    .append(text("Causing entity")))))
+                    .append(text(")")));
         return text.build();
     }
 }
